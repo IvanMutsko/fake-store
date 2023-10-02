2f790fd02dbe1c08e768059fa9cfeb57daaa149a
Ivan Mutsko <imucko1996@gmail.com>
2023-10-02 20:08:42 +0300

    complite category logic

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -12,34 +12,48 @@ const Category = () => {
 
   const defaultValues = {
     title: "",
-    price_min: 0,
+    price_min: 1, //API bug. When zero - returns all results
     price_max: 99999,
   };
 
   const defaultParams = {
     ...defaultValues,
+    limit: 10,
+    offset: 0,
     categoryId: id,
   };
 
-  const [category, setCategory] = useState("");
+  const [isEnd, setEnd] = useState(false);
+  const [items, setItems] = useState([]);
+  const [category, setCategory] = useState(null);
   const [values, setValues] = useState(defaultValues);
   const [params, setParams] = useState(defaultParams);
+
+  const { data = [], isLoading, isSuccess } = useGetProductsQuery(params);
+
+  useEffect(() => {
+    if (!id) return;
+
+    setValues(defaultValues);
+    setItems([]);
+    setEnd(false);
+    setParams({ ...defaultParams, categoryId: id });
+  }, [id]);
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (!data.length) return setEnd(true);
+
+    setItems((_items) => [..._items, ...data]);
+  }, [data, isLoading]);
 
   useEffect(() => {
     if (!id || !list.length) return;
 
     const category = list.find((item) => item.id === Number(id));
 
-    setCategory(category.name);
+    setCategory(category);
   }, [id, list]);
-
-  useEffect(() => {
-    if (!id) return;
-
-    setParams({ ...defaultParams, categoryId: id });
-  }, [id]);
-
-  const { data, isLoading, isSuccess } = useGetProductsQuery(params);
 
   const handleChange = ({ target: { value, name } }) => {
     setValues({ ...values, [name]: value });
@@ -48,12 +62,14 @@ const Category = () => {
   const handleSubmit = (evt) => {
     evt.preventDefault();
 
-    setParams({ ...params, ...values });
+    setItems([]);
+    setEnd(false);
+    setParams({ ...defaultParams, ...values });
   };
 
   return (
     <section>
-      <h2>{category}</h2>
+      <h2>{category?.name}</h2>
 
       <form onSubmit={handleSubmit}>
         <div>
@@ -84,13 +100,24 @@ const Category = () => {
 
       {isLoading ? (
         <p>Loading...</p>
-      ) : !isSuccess || !data.length ? (
+      ) : !isSuccess || !items.length ? (
         <div>
           <p>No result</p>
           <button type="click">Reset search</button>
         </div>
       ) : (
-        <Products title="" products={data} amount={data.length} />
+        <Products title="" products={items} amount={items.length} />
+      )}
+
+      {!isEnd && (
+        <button
+          type="click"
+          onClick={() =>
+            setParams({ ...params, offset: params.offset + params.limit })
+          }
+        >
+          Load more...
+        </button>
       )}
     </section>
   );
